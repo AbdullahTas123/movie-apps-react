e75119f91abcd8db3fd10f3c54ff16e872258dba
AbdullahTas123 <73878344+AbdullahTas123@users.noreply.github.com>
2022-08-16 10:58:44 +0300

    project done

--- src/App.js
+++ src/App.js
@@ -3,31 +3,61 @@ import React, { useState, useEffect } from "react";
 import { Movie } from "./components/Movie";
 
 const api_key = "aef71e4d18584f472147bed4873953bf";
-
 const FEATURED_API = "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=" + api_key + "&page=1";
 const SEARCH_API = "https://api.themoviedb.org/3/search/movie?api_key=" + api_key + "&query=";
 
-
 function App() {
   const [movies, setMovies] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
 
   useEffect(() => {
-    fetch(FEATURED_API)
+    getMovies(FEATURED_API);
+  }, []);
+
+  const getMovies = (API) => {
+    fetch(API)
       .then(res => res.json())
       .then(data => {
         console.log(data);
         setMovies(data.results);
       });
+  }
 
-  }, []);
+  const handleOnSubmit = e => {
+    e.preventDefault();
+
+    if (searchTerm) {
+      getMovies(SEARCH_API+searchTerm);
+      setSearchTerm("");
+    }
+  }
+
+  const handleOnChange = e => {
+    setSearchTerm(e.target.value);
+  }
 
   return (
-    <div>
-      {movies.length > 0 && movies.map(movie => 
-        <Movie key={movie.id} movie={movie} api_key={api_key} />
-      )}
-
-    </div>
+    <>
+      <header>
+        <a className="home" onClick={() => getMovies(FEATURED_API)}>Home</a>
+        <form onSubmit={handleOnSubmit}>
+          <input 
+            type="search" 
+            className="search" 
+            placeholder="Search..." 
+            value={searchTerm}
+            onChange={handleOnChange} 
+            
+            />
+        </form>
+      </header>
+      
+      <div className="movie-container" >
+        {movies.length > 0 && movies.map(movie => 
+          <Movie key={movie.id} movie={movie} api_key={api_key} />
+        )}
+      </div>
+    </>
   );
 }
 
--- src/components/Movie.js
+++ src/components/Movie.js
@@ -2,19 +2,30 @@ import React from 'react';
 
 const IMG_API = "https://image.tmdb.org/t/p/w1280";
 
+const setVoteClass = (vote) => {
+  if (vote >= 8) {
+    return "green";
+  } else if (vote >=6) {
+    return "orange";
+  } else {
+    return "red";
+  }
+}
 
 export const Movie = ({movie, api_key}) => {
-
   return (
     <div className='movie'>
       <div className="movie-header">
         <img src={IMG_API + movie.poster_path} alt={movie.title} />
         <div className="movie-info">
           <h3>{movie.title}</h3>
-          <span>{movie.vote_average}</span>
+          <span className={"vote " + setVoteClass(movie.vote_average)}>{movie.vote_average}</span>
+        </div>
+        <div className="movie-over">
+          <h2>Overview</h2>
+          <p>{movie.overview}</p>
         </div>
       </div>
-      
     </div>
   )
 }
