752d8ecaa38aa6eacb64f2093ea2954165559367
AbdullahTas123 <73878344+AbdullahTas123@users.noreply.github.com>
2022-08-16 11:19:18 +0300

    project done

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 //import { nanoid } from "nanoid";
 import { Movie } from "./components/Movie";
 
-const api_key = "aef71e4d18584f472147bed4873953bf";
+const api_key = ""; // get api key from The Movie Database
 const FEATURED_API = "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=" + api_key + "&page=1";
 const SEARCH_API = "https://api.themoviedb.org/3/search/movie?api_key=" + api_key + "&query=";
 
@@ -51,7 +51,7 @@ function App() {
             />
         </form>
       </header>
-      
+
       <div className="movie-container" >
         {movies.length > 0 && movies.map(movie => 
           <Movie key={movie.id} movie={movie} api_key={api_key} />
